3b95a580a8f07243777e2f492fa5fafa5ccd13d9
LeeHeoJae <heejae5599@gmail.com>
2026-05-12 13:58:53 +0900

    Revert "PERF: 필요 없는 접속을 삭제하여 로그인 성능 개선"
    - 로그인을 실패해도 성공했다고 착각하는 오류 유발
    
    This reverts commit be60421e286d8e9dcc52e228a256ade26581ed04.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -10,7 +10,17 @@ class AuthService {
     try {
       final loginData = {'USER_ID': studentId, 'PASSWD': password};
       await dio.get('https://my.hongik.ac.kr/my/login.do');
-      logMsg('쿠키 탈취');
+      logMsg('1단계 SSO 서버로 인증 시작');
+      final ssoResponse = await dio.post(
+        'https://ap.hongik.ac.kr/login/LoginCheck_SSO.php',
+        data: loginData,
+      );
+      logMsg('1단계 응답 : ${ssoResponse.data}');
+      if (ssoResponse.data['result_code'] == 'R') {
+        logMsg('로그인 차단됨: ${ssoResponse.data['result_msg']}');
+        return ssoResponse.data['result_msg'];
+      }
+      logMsg('2단계 쿠키 탈취');
       final classNetResponse = await dio.post(
         'https://ap.hongik.ac.kr/login/LoginExec3.php',
         data: loginData,
@@ -21,7 +31,7 @@ class AuthService {
           },
         ),
       );
-      logMsg('1단계 응답 : ${classNetResponse.data}');
+      logMsg('2단계 응답 : ${classNetResponse.data}');
       final htmlBody = classNetResponse.data.toString();
 
       // SetCookie('이름', '값'...) 패턴을 찾는 정규식
@@ -51,7 +61,7 @@ class AuthService {
         extractedCookies,
       );
 
-      logMsg('2단계 : 출결 서버 세션 활성화 시작');
+      logMsg('3단계 : 출결 서버 세션 활성화 시작');
       await dio.get(
         'https://at.hongik.ac.kr/login.jsp',
         options: Options(headers: {'Referer': 'https://my.hongik.ac.kr/'}),
